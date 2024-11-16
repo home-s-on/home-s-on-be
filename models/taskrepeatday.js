82bda49f0256a1f96782ae1b84ea3bf5128a204b
mjpark-dot <majongyi@gamil.com>
2024-11-16 11:54:09 +0900

    feat: create tasks, taskrepeatdays table #1

---
+++ models/taskrepeatday.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class TaskRepeatDay extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsTo(models.Task, { foreignKey:"task_id", targetKey:"id"})
+    }
+  }
+  TaskRepeatDay.init({
+    day_of_week: {
+      type: DataTypes.INTEGER,
+     // primaryKey: true,
+      allowNull: false,
+      validate : {
+        min: 0,
+        max: 6
+      },
+    }
+  }, {
+    sequelize,
+    modelName: 'TaskRepeatDay',
+    tableName: 'taskrepeatdays',
+    timestamps: true,
+  });
+  return TaskRepeatDay;
+};
