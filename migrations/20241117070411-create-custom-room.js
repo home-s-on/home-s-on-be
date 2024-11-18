9bbeae069d7b8bbb0a53e0063a924616461cd8c1
etesongg <etesong0114@gmail.com>
2024-11-18 11:13:57 +0900

    feat : migrations 파일에 up, down tablename 동일하게 수정

--- migrations/20241117070411-create-custom-room.js
+++ migrations/20241117070411-create-custom-room.js
@@ -28,6 +28,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("CustomRooms");
+    await queryInterface.dropTable("customrooms");
   },
 };
