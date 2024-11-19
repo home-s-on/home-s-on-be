b271b5449aeb9d88c5b8d5713432502189e8abb6
mjpark-dot <majongyi@gmail.com>
2024-11-20 02:13:11 +0900

    feat : API  House 의 create , getInviteCode 구현 ( houses, userhouses table) #5

--- migrations/20241114154844-create-house.js
+++ migrations/20241114154844-create-house.js
@@ -10,7 +10,7 @@ module.exports = {
         type: Sequelize.INTEGER,
       },
       invite_code: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.STRING,
         allowNull: false,
       },
       createdAt: {
