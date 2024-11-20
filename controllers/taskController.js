964069572555fc74b6d7e8805a7127bc8f2360e5
Ahj-hj <ahj32@hanmail.net>
2024-11-20 16:40:53 +0900

    feat:task-list myTask #4

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -61,3 +61,63 @@ exports.getAllTasksByHouseId = async (req, res) => {
     res.status(500).json({ success: false, error: "Internal server error" });
   }
 };
+
+// <<나의 할일 보기>>
+exports.getMyTasks = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    if (!userId) {
+      return res
+        .status(400)
+        .json({ success: false, error: "사용자 ID가 필요합니다." });
+    }
+
+    // 사용자의 house_id찾기
+    const userHouse = await UserHouse.findOne({
+      where: { user_id: userId },
+      attributes: ["house_id"],
+    });
+
+    if (!userHouse) {
+      return res.status(404).json({
+        success: false,
+        error:
+          "사용자의 집을 찾을 수 없습니다. 사용자가 집에 등록되어있는지 확인해주세요.",
+      });
+    }
+
+    // 해당 house_id의 모든 할일 가져오기
+    const allTasks = await Task.findAll({
+      where: {
+        house_id: userHouse.house_id,
+      },
+      include: [
+        {
+          model: HouseRoom,
+          attributes: ["id", "room_name"],
+        },
+      ],
+      order: [["createdAt", "DESC"]],
+    });
+
+    // JavaScript 사용 -> 사용자의 할일 만 필터링..
+    const tasks = allTasks.filter(
+      (task) => task.assignee_id.includes(parseInt(userId)) //문자열 -> 정수반환
+    );
+
+    if (tasks.length === 0) {
+      return res
+        .status(404)
+        .json({ success: false, error: "사용자의 할일 목록이 비어있습니다." });
+    }
+
+    res.json({ success: true, data: tasks });
+  } catch (error) {
+    console.error("할일 목록을 가져올 수 없습니다:", error);
+    res.status(500).json({
+      success: false,
+      error: "Internal server error",
+    });
+  }
+};
--- routers/taskRouter.js
+++ routers/taskRouter.js
@@ -4,5 +4,7 @@ const taskController = require("../controllers/taskController");
 
 //사용자 집의 모든 할 일
 router.get("/house/:houseId", taskController.getAllTasksByHouseId);
+// 나의 할 일
+router.get("/mytasks/:userId", taskController.getMyTasks);
 
 module.exports = router;
