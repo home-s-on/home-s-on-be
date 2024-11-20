bd0716e454061cdb17c099fc03e36e1f8a8f1e1e
etesongg <etesong0114@gmail.com>
2024-11-20 10:47:35 +0900

    feat : email login #10 - 토큰 발급 및 로그인 성공

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,34 @@
-const User = require("../models/User");
+const { User } = require("../models");
+const bcrypt = require("bcryptjs");
 
 const userController = {};
+
+userController.createUser = async (req, res) => {
+  try {
+    let { email, password } = req.body;
+
+    if (!email || !password) {
+      throw new Error("Email and password are required");
+    }
+
+    const user = await User.findOne({ where: { email } });
+    if (user) {
+      throw new Error("User already exists");
+    }
+    const salt = await bcrypt.genSalt(10);
+    password = await bcrypt.hash(password, salt);
+    await User.create({
+      email,
+      password,
+      nickname: email.split("@")[0],
+      social_login_type: "EMAIL",
+    });
+
+    return res.status(201).json({ status: "success" });
+  } catch (e) {
+    res.status(400).json({ status: "fail", message: e.message });
+  }
+};
 
 userController.getUser = async (req, res) => {
   try {
--- models/user.js
+++ models/user.js
@@ -1,13 +1,18 @@
 "use strict";
 const { Model } = require("sequelize");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    async generateToken() {
+      const token = jwt.sign({ id: this.id }, JWT_SECRET_KEY, {
+        expiresIn: "1d",
+      });
+      return token;
+    }
+
     static associate(models) {
       User.hasOne(models.UserHouse, {
         foreignKey: "user_id",
@@ -23,33 +28,30 @@ module.exports = (sequelize, DataTypes) => {
         autoIncrement: true,
         allowNull: false,
       },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
       password: {
         type: DataTypes.STRING,
         allowNull: false,
       },
       nickname: {
         type: DataTypes.STRING,
-        allowNull: false,
       },
       profile_img_url: {
         type: DataTypes.STRING,
-        allowNull: false,
       },
       social_login_type: {
-        type: DataTypes.ENUM("NAVER", "KAKAO", "GOOGLE", "APPLE"),
+        type: DataTypes.ENUM("EMAIL", "KAKAO", "GOOGLE", "APPLE"),
         allowNull: false,
-      },
-      account_id: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
       },
     },
     {
       sequelize,
       modelName: "User",
       tableName: "users",
-      timestamps: true,
+      timestamps: false,
     }
   );
 
