a0ed3c43e14473e3ad631999112d2a404b1ca90e
etesongg <etesong0114@gmail.com>
2024-12-05 14:10:09 +0900

    test

--- config/config.js
+++ config/config.js
@@ -8,6 +8,7 @@ module.exports = {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
   },
+
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
