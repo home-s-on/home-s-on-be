ef97dc8aab20764d609ae75a40a1c3c0ff7fceb9
Ahj-hj <ahj32@hanmail.net>
2024-12-10 01:15:53 +0900

    feat:할일삭제 -라우터수정#64

--- routers/taskRouter.js
+++ routers/taskRouter.js
@@ -21,11 +21,6 @@ router.get(
 router.post("/add", authController.authenticate, taskController.addTask);
 //할 일 편집
 router.patch("/:taskId", authController.authenticate, taskController.editTask);
-// router.put(
-//   "/edit/:taskId",
-//   authController.authenticate,
-//   taskController.editTask
-// );
 
 //할 일 완료
 router.patch(
@@ -36,7 +31,7 @@ router.patch(
 
 //할 일 삭제
 router.delete(
-  "/delete/:taskId",
+  "/:taskId",
   authController.authenticate,
   taskController.deleteTask
 );
