04854eb4b836c93a55d3c0d4f36b43311deb9c0f
etesong014 <55964387+etesongg@users.noreply.github.com>
2024-12-13 00:53:19 +0900

    feat : 일주일치 task를 받아서 알림으로 반응해주는 컨트롤러 구현#75

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -1,6 +1,7 @@
-const { User, ChatHistory } = require("../models");
+const { ChatHistory, Task } = require("../models");
 const { OpenAI } = require("openai");
 require("dotenv").config();
+const { Op } = require("sequelize");
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -76,6 +77,59 @@ chatController.getChat = async (req, res) => {
         existingChat,
       },
     });
+  } catch (e) {
+    return res.status(400).json({ status: "fail", message: e.message });
+  }
+};
+
+chatController.motivationChat = async (req, res) => {
+  try {
+    const { userId } = req;
+
+    const tasks = await Task.findAll({
+      where: {
+        user_id: userId,
+        due_date: {
+          [Op.gte]: new Date(new Date().setDate(new Date().getDate() - 7)),
+        },
+      },
+    });
+
+    const completedTasks = tasks.filter((task) => task.complete);
+    const completionRate = (completedTasks.length / tasks.length) * 100 || 0;
+
+    let motivationMessage;
+
+    if (completionRate >= 80) {
+      motivationMessage =
+        "훌륭해요! 이번 주에 많은 일을 완료하셨습니다. 계속 이렇게 잘 해보세요!";
+    } else if (completionRate >= 50) {
+      motivationMessage =
+        "좋아요! 절반 이상 완료하셨습니다. 조금 더 힘내서 목표를 달성해보세요!";
+    } else {
+      const incompleteTaskTitles = tasks
+        .filter((task) => !task.complete)
+        .map((task) => task.title);
+      motivationMessage = `이번 주에 완료된 작업은 적지만, ${incompleteTaskTitles[0]} 하나라도 완료하면 큰 도움이 될 거예요!`;
+    }
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: [
+        {
+          role: "system",
+          content:
+            "너는 사용자에게 일주일치 집안일에 대한 상황을 받아서 상황에 맞는 동기부여 메시지를 주는 역할을 가지고 있어. 메시지는 긍정적이고 간결하며, 사용자가 집안일을 완료할 수 있도록 격려하는 내용을 포함했으면 좋겠어. 20자를 넘지 않고 사용자에게 친근감을 줄 수 있는 어조를 사용해줘.",
+        },
+        {
+          role: "user",
+          content: motivationMessage,
+        },
+      ],
+    });
+    const aiMessage = response.choices[0].message.content;
+
+    return res.status(200).json({ status: "success", message: aiMessage });
   } catch (e) {
     return res.status(400).json({ status: "fail", message: e.message });
   }
